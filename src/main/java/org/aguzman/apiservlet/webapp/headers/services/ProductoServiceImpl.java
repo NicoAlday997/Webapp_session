231064bbc76f2b32608102a1806f475b8af5031d
NicolasAlday <alday997@gmail.com>
2024-02-06 16:58:56 -0600

    Subida de proyecto

---
+++ src/main/java/org/aguzman/apiservlet/webapp/headers/services/ProductoServiceImpl.java
@@ -0,0 +1,41 @@
+package org.aguzman.apiservlet.webapp.headers.services;
+
+import org.aguzman.apiservlet.webapp.headers.models.Producto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductoServiceImpl implements ProductoService{
+
+    @Override
+    public List<Producto> listar() {
+        return Arrays.asList(new Producto(1L, "notebook", "computacion", 17500),
+                new Producto(2L, "mesa escritorio", "oficina", 100000),
+                new Producto(3L, "teclado mecanico", "computacion", 40000));
+    }
+
+    @Override
+    public Optional<Producto> porId(Long id) {
+        return listar().stream().filter(p->p.getId().equals(id)).findAny();
+    }
+
+    /*@Override
+    public Optional<Producto> buscarProducto(String nombre) {
+
+        return listar().stream().filter(p -> {
+            if (nombre == null || nombre.isBlank()) {
+                return false;
+            }
+            return p.getNombre().contains(nombre);
+        }).findFirst();*/
+
+       /* return listar().stream()
+                .filter(p -> nombre != null && !nombre.isBlank() && p.getNombre().contains(nombre))
+                .findFirst();*/
+
+    //}
+
+
+
+}
